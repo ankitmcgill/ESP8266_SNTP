048106b4b8fbfffd4d30e7a500d4858e240645ab
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-10-27 17:49:48 +0530

    initial source/header file skeleton commit

---
+++ ESP8266_SNTP.c
@@ -0,0 +1,9 @@
+/*************************************************
+* ESP8266 SNTP LIBRARY
+*
+* OCTOBER 27 2016
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+* ***********************************************/
+
+#include "ESP8266_SNTP.h"
---
+++ ESP8266_SNTP.h
@@ -0,0 +1,12 @@
+/*************************************************
+* ESP8266 SNTP LIBRARY
+*
+* OCTOBER 27 2016
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+* ***********************************************/
+
+#ifndef _ESP8266_SNTP_H_
+#define _ESP8266_SNTP_H_
+
+#endif
