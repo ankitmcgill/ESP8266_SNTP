6973625363791795b1a2782feb36030399e82ccc
Ankit Bhatnagar <ankit.bhatnagar@semaconnect.com>
2017-08-17 00:40:31 +0530

    modified to bring up to coding standard of my libraries. confirmed tested working

--- ESP8266_SNTP.c
+++ ESP8266_SNTP.c
@@ -1,34 +1,86 @@
 /*************************************************
 * ESP8266 SNTP LIBRARY
 *
+*	REFERENCE
+* ----------
+*	(1) ONLINE NTP TOOL
+*			http://www.timestampconvert.com/
+*
+*	(2) NTP TOOL
+*			https://w3dt.net/tools/ntpq
+*
 * OCTOBER 27 2016
+*
 * ANKIT BHATNAGAR
 * ANKIT.BHATNAGARINDIA@GMAIL.COM
 * ***********************************************/
 
 #include "ESP8266_SNTP.h"
 
-os_timer_t sntp_done_check_timer;
+//LOCAL LIBRARY VARIABLES////////////////////////////////
+//DEBUG RELATED
+static uint8_t _esp8266_sntp_debug;
 
-void ESP8266_SNTP_set_server(uint8_t index, struct ip_addr* i)
+//TIMER RELATED
+static uint8_t esp8266_sntp_try_count;
+static os_timer_t _esp8266_sntp_done_check_timer;
+
+//CB FUNCTION
+static void (*_esp8266_sntp_user_cb)(uint32_t);
+//END LOCAL LIBRARY VARIABLES/////////////////////////////
+
+void ICACHE_FLASH_ATTR ESP8266_SNTP_SetDebug(uint8_t debug_on)
+{
+	//SET DEBUG PRINTF ON(1) OR OFF(0)
+
+	_esp8266_sntp_debug = debug_on;
+}
+
+void ICACHE_FLASH_ATTR ESP8266_SNTP_SetServerIp(uint8_t index, struct ip_addr* i)
 {
 	//SET THE SNTP TIME SERVER BASED ON IP ADDRESS SUPPLIED
 	//ESP8266 CAN HAVE 3 TIME SERVER CONFIGURED
 	// 0 <= index <= 2
 
+	if(index > 2)
+	{
+		if(_esp8266_sntp_debug)
+		{
+			os_printf("ESP8266 : SNTP : Server index greater than 2 not allowed\n");
+		}
+		return;
+	}
+
 	sntp_setserver(index, i);
+	if(_esp8266_sntp_debug)
+	{
+		os_printf("ESP8266 : SNTP : server index %d set to %s\n", index, IP2STR(i));
+	}
 }
 
-void ESP8266_SNTP_set_server_name(uint8_t index, uint8_t* server_name)
+void ICACHE_FLASH_ATTR ESP8266_SNTP_SetServerName(uint8_t index, uint8_t* server_name)
 {
 	//SET THE SNTP TIME SERVER BASED ON NAMED TIME SERVER SUPPLIED
 	//ESP8266 CAN HAVE 3 TIME SERVER CONFIGURED
 	// 0 <= index <= 2
 
+	if(index > 2)
+	{
+		if(_esp8266_sntp_debug)
+		{
+			os_printf("ESP8266 : SNTP : Server index greater than 2 not allowed\n");
+		}
+		return;
+	}
+
 	sntp_setservername(index, server_name);
+	if(_esp8266_sntp_debug)
+	{
+		os_printf("ESP8266 : SNTP : server index %d set to %s\n", index, server_name);
+	}
 }
 
-void ESP8266_SNTP_set_timezone(int8_t utc_offset_hours)
+void ICACHE_FLASH_ATTR ESP8266_SNTP_SetTimezone(int8_t utc_offset_hours)
 {
 	//SET TIMEZONE OFFFSET FROM UTC
 	//USED BY ESP8266 TO CALCULATE LOCAL TIME AS NTP RETURNS
@@ -36,51 +88,120 @@ void ESP8266_SNTP_set_timezone(int8_t utc_offset_hours)
 	//RIGHT NOW ONLY SUPPORTS HOURS OFFSET
 
 	sntp_set_timezone(utc_offset_hours);
+	if(_esp8266_sntp_debug)
+	{
+		os_printf("ESP8266 : SNTP : timezone offset set to %d\n", utc_offset_hours);
+	}
 }
 
-void ESP8266_SNTP_start(void)
+void ICACHE_FLASH_ATTR ESP8266_SNTP_SetCbFunction(void (*user_cb_fn)(uint32))
+{
+	//SET THE USER CB FUNCTION
+
+	_esp8266_sntp_user_cb = user_cb_fn;
+	if(_esp8266_sntp_debug)
+	{
+		os_printf("ESP8266 : SNTP : user cb function set\n");
+	}
+}
+
+void ICACHE_FLASH_ATTR ESP8266_SNTP_Start(void)
 {
 	//START SNTP PROCESS OF CONNECTING TO THE TIME SERVER
 	//AND GETTING THE TIME
 
 	sntp_init();
+	if(_esp8266_sntp_debug)
+	{
+		os_printf("ESP8266 : SNTP : Started\n");
+	}
 }
 
-void ESP8266_SNTP_get_current_timestamp(void)
+void ICACHE_FLASH_ATTR ESP8266_SNTP_Stop(void)
+{
+	//STOP SNTP
+
+	sntp_stop();
+	if(_esp8266_sntp_debug)
+	{
+		os_printf("ESP8266 : SNTP : Stopped\n");
+	}
+}
+
+uint32_t ICACHE_FLASH_ATTR ESP8266_SNTP_GetCurrentTimestamp(void)
 {
 	//RETURN THE CURRENT LOCAL TIMESTAMP FROM
 	//JANUARY 1 1970
 
-	os_timer_setfn(&sntp_done_check_timer, ESP8266_SNTP_timer_cb, NULL);
+	uint32_t ts = sntp_get_current_timestamp();
+	if(ts == 0)
+	{
+		//INVALID TIMESTAMP
+		//START TIMER TO GET TIMESTAMP AGAIN
+		if(_esp8266_sntp_debug)
+		{
+			os_printf("ESP8266 : SNTP : First try unsuccessfull. Starting timer to try again\n");
+		}
+		esp8266_sntp_try_count = 0;
+		os_timer_setfn(&_esp8266_sntp_done_check_timer, _esp8266_sntp_timer_cb, NULL);
+		os_timer_arm(&_esp8266_sntp_done_check_timer, 500, 0);
+	}
+	else
+	{
+		//TIMESTAMP IS VALID
+		return ts;
+	}
 
-	//NEED TO GIVE SNTP MORE TIME
-	os_timer_arm(&sntp_done_check_timer, 500, 1);
+	return 0;
 }
 
-void ESP8266_SNTP_get_time_string(void)
+char* ICACHE_FLASH_ATTR ESP8266_SNTP_GetTimeString(uint32_t timestamp)
 {
-	//GET THE CURRENT LOCAL TIME AS A STRING
+	//RETURN THE CURRENT TIME STRING IN SPECIFIED FORMAT
+	//FROM THE SUPPLIED TIMESTAMP
+
+	return sntp_get_real_time(timestamp);
 }
 
-void ESP8266_SNTP_set_callback(void (*func)(uint32))
+void ICACHE_FLASH_ATTR _esp8266_sntp_timer_cb(void *pArg)
 {
-	//SET THE CALLBACK FUNCTION THAT THE LIBRARY WILL CALL ONCE A VALID TIME IS
-	//IS RECEIVED FROM THE TIME SERVER. CURRENT TIMESTAMP WILL BE PASSED TO
-	//CALLBACK FUNCTION AS ARGUMENT
+	//INTERNAL TIMER CB FUNCTION
 
-	ESP8266_SNTP_time_operation_done_callback = func;
-}
-
-void ESP8266_SNTP_timer_cb(void* pArg)
-{
-	os_printf("sntp timer tick\n");
+	esp8266_sntp_try_count++;
 	uint32_t val = sntp_get_current_timestamp();
+
 	if(val != 0)
 	{
-		os_printf("sntp timer done\n");
 		//SNTP TIME IS READY
-		os_timer_disarm(&sntp_done_check_timer);
-		//CALL THE USER SPECIFIED CALLBACK FUNCTION
-		ESP8266_SNTP_time_operation_done_callback(val);
+		os_timer_disarm(&_esp8266_sntp_done_check_timer);
+		if(_esp8266_sntp_debug)
+		{
+			os_printf("ESP8266 : SNTP : Got timestamp @ attempt #%u\n", esp8266_sntp_try_count);
+		}
+		//CALL THE USER SPECIFIED CALLBACK FUNCTION IF NOT NULL
+		if(_esp8266_sntp_user_cb != NULL)
+		{
+			(*_esp8266_sntp_user_cb)(val);
+		}
+	}
+	else
+	{
+		if(esp8266_sntp_try_count > ESP8266_SNTP_RETRIES_COUNT)
+		{
+			//MAX RETRY COUNT EXCEEDED
+			os_timer_disarm(&_esp8266_sntp_done_check_timer);
+			if(_esp8266_sntp_debug)
+			{
+				os_printf("ESP8266 : SNTP : All tries finished. Calling user cb with timestamp = 0\n", esp8266_sntp_try_count);
+			}
+			if(_esp8266_sntp_user_cb != NULL)
+			{
+				(*_esp8266_sntp_user_cb)(0);
+			}
+		}
+		else
+		{
+			os_timer_arm(&_esp8266_sntp_done_check_timer, 500, 0);
+		}
 	}
 }
--- ESP8266_SNTP.h
+++ ESP8266_SNTP.h
@@ -1,7 +1,16 @@
 /*************************************************
 * ESP8266 SNTP LIBRARY
 *
+*	REFERENCE
+* ----------
+*	(1) ONLINE NTP TOOL
+*			http://www.timestampconvert.com/
+*
+*	(2) NTP TOOL
+*			https://w3dt.net/tools/ntpq
+*
 * OCTOBER 27 2016
+*
 * ANKIT BHATNAGAR
 * ANKIT.BHATNAGARINDIA@GMAIL.COM
 * ***********************************************/
@@ -14,20 +23,24 @@
 #include "user_interface.h"
 #include <ets_sys.h>
 
-os_timer_t sntp_done_check_timer;
-struct ip_addr ip;
+#define ESP8266_SNTP_RETRIES_COUNT 5
 
-//POINTER TO THE CALLBACK FUNCTION THAT THE LIBRARY WILL CALL
-//AFTER SNTP OPERATION DONE AND PASS THE CALCULATED TIMESTAMP AS ARGUMENT
-void (*ESP8266_SNTP_time_operation_done_callback)(uint32);
+//FUNCTION PROTOTYPES/////////////////////////////////////////////
+//CONFIGURATION FUNCTIONS
+void ICACHE_FLASH_ATTR ESP8266_SNTP_SetDebug(uint8_t debug_on);
+void ICACHE_FLASH_ATTR ESP8266_SNTP_SetServerIp(uint8_t index, struct ip_addr* i);
+void ICACHE_FLASH_ATTR ESP8266_SNTP_SetServerName(uint8_t index, uint8_t* server_name);
+void ICACHE_FLASH_ATTR ESP8266_SNTP_SetTimezone(int8_t utc_offset_hours);
+void ICACHE_FLASH_ATTR ESP8266_SNTP_SetCbFunction(void (*user_cb_fn)(uint32));
 
-void ESP8266_SNTP_set_server(uint8_t index, struct ip_addr* i);
-void ESP8266_SNTP_set_server_name(uint8_t index, uint8_t* server_name);
-void ESP8266_SNTP_set_timezone(int8_t utc_offset_hours);
-void ESP8266_SNTP_start(void);
-void ESP8266_SNTP_get_current_timestamp(void);
-void ESP8266_SNTP_get_time_string(void);
-void ESP8266_SNTP_set_callback(void (*func)(uint32));
-void ESP8266_SNTP_timer_cb(void *pArg);
+//OPERATION FUNCTIONS
+void ICACHE_FLASH_ATTR ESP8266_SNTP_Start(void);
+void ICACHE_FLASH_ATTR ESP8266_SNTP_Stop(void);
 
+//GET FUNCTIONS
+uint32_t ICACHE_FLASH_ATTR ESP8266_SNTP_GetCurrentTimestamp(void);
+char* ICACHE_FLASH_ATTR ESP8266_SNTP_GetTimeString(uint32_t timestamp);
+
+//INTERNAL FUNCTIONS
+void ICACHE_FLASH_ATTR _esp8266_sntp_timer_cb(void *pArg);
 #endif
